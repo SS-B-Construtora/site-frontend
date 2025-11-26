977167713f9e29f4555643f3f613e7c1dd0c105c
Gabriel Moreira <gmdoas.dev@gmail.com>
2025-11-26 15:54:01 -0300

    ..

--- src/components/Home/NewsCarousel.tsx
+++ src/components/Home/NewsCarousel.tsx
@@ -97,7 +97,8 @@ function NewsCarousel() {
                     <h3 className="font-bold text-lg mb-2 line-clamp-2">
                       {item.title}
                     </h3>
-                    <button className="rounded-full w-full text-gray-300 border border-white  mb-2 rounded-md mt-4 hover:text-white hover:bg-[#A1C737] transition-colors duration-200 font-bold">
+                    <button className="rounded-full w-full text-gray-300 border border-white  mb-2 rounded-md mt-4 hover:text-white hover:bg-[#A1C737] transition-colors duration-200 font-bold"
+                    onClick={() => navigate(`/noticias/detalhes?id=${item.id}`)}>
                       Leia mais
                     </button>
                   </div>
